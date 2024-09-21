984cee4871e348c7ee80a2b0d90ec24d26b0a655
Virecny Stephanie Mata Toyo <162191678+Stephaniemtoyo@users.noreply.github.com>
2024-09-21 15:04:26 +0200

    Initial commit

---
+++ src/js/component/home.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+
+//include images into your bundle
+import rigoImage from "../../img/rigo-baby.jpg";
+
+//create your first component
+export function Home() {
+const [ selectedColor, setSelectedColor ] = useState("green");
+
+	return (
+		<div className="traffic-light">
+			<div 
+			onClick={() => setSelectedColor("red")}
+			className={"light red"+((selectedColor === "red") ? " glow" : "")}></div>
+			<div 
+			onClick={() => setSelectedColor("yellow")}
+			className={"light yellow"+((selectedColor === "yellow") ? " glow" : "")}></div>
+			<div 
+			onClick={() => setSelectedColor("green")}
+			className={"light green"+((selectedColor === "green") ? " glow" : "")}></div>
+			
+		</div>
+	);
+}
+export default Home;
